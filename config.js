abb0c5c89fed38fb7c7e508dc3951f8e2d87efa1
albertoquintanainf <alberto.quintanainf@gmail.com>
2026-09-24 10:05:57 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -12,7 +12,7 @@
 window.CONFIG = {
   NOMBRE_CLASE: "El cante a cuchillo",
   PROFESOR: "El Cuchillero",
-  EMAIL_SUGERENCIAS: "alberto.quintanainf@gmail.com",  // a dónde llegan las sugerencias
+  ADMIN_PASSWORD: "tirititran",
 
   SUPABASE_URL: "https://hrrlgriqtasuueimebei.supabase.co",
   SUPABASE_ANON_KEY: "sb_publishable_vxF-17T5ITLaXDDkq6MT5A_ORbSi7PK"
